82d1b88125e264ef6b0fd476af43ef32c37a4c01
Philip Ngo <ngo12@college.harvard.edu>
2013-04-25 14:16:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WolframalphaTest::Application.config.secret_token = 'd74edd0f046e96e3cb4490c233e8f254b3d1518bd04d872c6f0f888f008596945328084a58c5820f057e90df8e4b6f7d74a5fcc56ab46769cf93c88c4468863c'
